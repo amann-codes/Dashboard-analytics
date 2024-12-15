82168d596afbc9df58cde1a0d80a9761ba73ff63
amanprajapati00 <aman.prajapati3000@gmail.com>
2024-12-15 18:29:46 +0530

    redirect from /dashboard to /dashbord/summary

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/dashboard",
+        destination: "/dashboard/summary",
+        permanent: true,
+      },
+      {
+        source: "/dashboard",
+        destination: "/dashboard/summary",
         permanent: true,
       },
     ];
